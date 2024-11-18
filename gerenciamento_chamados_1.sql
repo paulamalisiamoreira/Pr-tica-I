322ac6098c42d76059cd90f4805cd97114e20c6a
Paula Malisia Moreira - <142233587+paulamalisiamoreira@users.noreply.github.com>
2024-11-18 08:17:43 -0300

    Add files via upload

---
+++ gerenciamento_chamados_1.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE gerenciamento_chamados_1;
+USE gerenciamento_chamados_1;
+
+CREATE TABLE cliente(
+pk_cliente  int primary key not null auto_increment,
+nome_cliente varchar(50),
+email_cliente varchar(50),
+telefone_cliente varchar(11)
+);
+
+CREATE TABLE colaborador(
+pk_colaborador int primary key not null auto_increment,
+nome_colaborador varchar(50),
+email_colaborador varchar(50),
+telefone_colaborador varchar(11)
+);
+
+CREATE TABLE chamado(
+pk_chamado int primary key not null auto_increment,
+fk_cliente int not null,
+FOREIGN KEY (fk_cliente) REFERENCES cliente(pk_cliente),
+fk_colaborador int not null,
+FOREIGN KEY (fk_colaborador) REFERENCES colaborador(pk_colaborador),
+descricao varchar(500),
+criticidade varchar(15),
+estatus varchar(20),
+data_abertura date
+);
